664aec8bb7efc911ab35cea870c2934ff491667c
Deepma1283 <jaiswaldeepma1283@gmail.com>
2025-08-03 12:02:38 +0000

    datatypes done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,33 @@
+"use strict"; // this indicates that the entire js code hs to be treated as per the new version of javascript
+
+// alert (3 + 3) 
+// alert works in browser there it will pop 6
+// alert doesn't work in case of node.js 
+
+let naam = "SUJAL"
+let age = 19
+let isLoggedIn = false
+let state;
+let randm = null 
+
+// JAVA SCRIPT IS DYNAMIC AND WEAKLY TYPED LANGUAGE
+
+
+// Primitive type are as follows :
+// number => it can store positive floating number upto 2 to power 53 and negative type floating value of same magnitude as well
+// bigint => operate on large integers even beyond the safe integer limit for Numbers.
+// String => " "
+// boolean => true/false
+// null type => null is a standalone value
+// undefined =>
+// symbol => unique
+
+
+// object 
+
+console.log(typeof naam)
+console.log(typeof age)
+console.log(typeof isLoggedIn)
+console.log(typeof state)
+console.log(typeof randm)
+
